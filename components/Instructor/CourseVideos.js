afd5800d52af37a29d248681c22afbd9aaff620f
Khushivithlani2 <khushivithlani2@gmail.com>
2023-02-24 18:24:07 +0530

    PAC 8 : Work on list view of Videos section on FACULTY side in frontend

--- components/Instructor/CourseVideos.js
+++ components/Instructor/CourseVideos.js
@@ -11,55 +11,6 @@ const CourseVideos = ({ id:videoId,sessionOrder, sessionTitle, isPreview, isPubl
 				<div className="create-course-form">
 					{console.log("VIdeos ----------------->isPublies:"+isPublished+"isPreview"+isPreview)}
 					<thead></thead>
-
-
-
-					{/*<div className="card" style={{ width: "18rem" }}>*/}
-					{/*	<img*/}
-					{/*		src={thumb ? thumb : ""}*/}
-					{/*		className="card-img-top"*/}
-					{/*		alt="..."*/}
-					{/*	/>*/}
-					{/*	<div className="card-body">*/}
-					{/*		<h5 className="card-title">{title}</h5>*/}
-					{/*		<button*/}
-					{/*			onClick={() => onDelete(videoId)}*/}
-					{/*			className="btn btn-danger"*/}
-					{/*		>*/}
-					{/*			Delete*/}
-					{/*		</button>*/}
-					{/*	</div>*/}
-					{/*</div>*/}
-
-					{/*<tbody>*/}
-					{/*{courses.length > 0 ? (*/}
-					{/*	courses.map((course) => (*/}
-					{/*		<CourseRow*/}
-					{/*			key={course.id}*/}
-					{/*			{...course}*/}
-					{/*			onHome={() =>*/}
-					{/*				handleCourseHome(*/}
-					{/*					course.id*/}
-					{/*				)*/}
-					{/*			}*/}
-					{/*			onHomeRemove={() =>*/}
-					{/*				handleCourseRemoveHome(*/}
-					{/*					course.id*/}
-					{/*				)*/}
-					{/*			}*/}
-					{/*		/>*/}
-					{/*	))*/}
-					{/*) : (*/}
-					{/*	<tr>*/}
-					{/*		<td*/}
-					{/*			colSpan="7"*/}
-					{/*			className="text-center py-3"*/}
-					{/*		>*/}
-					{/*			Empty!*/}
-					{/*		</td>*/}
-					{/*	</tr>*/}
-					{/*)}*/}
-					{/*</tbody>	*/}
 			<tbody className="tbody,td,tfoot,th,thead,tr">
 					<tr className="videoDetailrows">
 						<td scope="col" style={{width: "12rem"}}>{sessionOrder}</td>
