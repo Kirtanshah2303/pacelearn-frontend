69af49552dd6b4283207e663b382dbcb4dcf545a
Khushivithlani2 <khushivithlani2@gmail.com>
2023-02-24 18:22:16 +0530

    Add edit,delete and view functionally

--- components/Instructor/CourseVideos.js
+++ components/Instructor/CourseVideos.js
@@ -62,14 +62,23 @@ const CourseVideos = ({ id:videoId,sessionOrder, sessionTitle, isPreview, isPubl
 					{/*</tbody>	*/}
 			<tbody className="tbody,td,tfoot,th,thead,tr">
 					<tr className="videoDetailrows">
-						<td scope="col" >{sessionOrder}</td>
+						<td scope="col" style={{width: "12rem"}}>{sessionOrder}</td>
 						<td scope="col" style={{width: "15rem"}}>{sessionTitle} </td>
-						<td scope="col" >3 hr 50 min</td>
-						<td scope="col" >{isPreview.toString()}</td>
-						<td scope="col" >{isPublished.toString()}</td>
-						<td scope="col" >
+						<td scope="col" style={{width: "12rem"}}>3 hr 50 min</td>
+						<td scope="col" style={{width: "12rem"}}>{isPreview.toString()}</td>
+						<td scope="col" style={{width: "12rem"}}>{isPublished.toString()}</td>
+						<td scope="col" style={{width: "12rem"}}>
 							<div className="btn-group">
+								<button style={{background:"#008000"}} //onClick={() => onDelete(videoId)}
+										className="btn btn-danger">
+									View Content
 
+								</button>
+								<button style={{background:"#2780e3"}} //onClick={() => onDelete(videoId)}
+										className="btn btn-danger"> edit</button>
+
+								<button onClick={() => onDelete(videoId)}
+										className="btn btn-danger"> Delete</button>
 							</div>
 
 						</td>
