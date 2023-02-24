4d8318712df436d05d1673fa330fc56bae18e7e1
Khushivithlani2 <khushivithlani2@gmail.com>
2023-02-24 18:20:05 +0530

    fetch video detail in Table form

--- components/Instructor/CourseVideos.js
+++ components/Instructor/CourseVideos.js
@@ -1,26 +1,89 @@
 import React from "react";
+import CourseRow from "@/components/Admin/CourseRow";
 
-const CourseVideos = ({ id: videoId, title, thumb, video, onDelete }) => {
+const CourseVideos = ({ id:videoId,sessionOrder, sessionTitle, isPreview, isPublished, onDelete }) => {
+
+
 	return (
-		<div className="col-lg-3 col-md-6">
-			<div className="card" style={{ width: "18rem" }}>
-				<img
-					src={thumb ? thumb : ""}
-					className="card-img-top"
-					alt="..."
-				/>
-				<div className="card-body">
-					<h5 className="card-title">{title}</h5>
-					<button
-						onClick={() => onDelete(videoId)}
-						className="btn btn-danger"
-					>
-						Delete
-					</button>
+
+		<div className="table-responsive">
+			<table className="table align-middle table-hover fs-14" >
+				<div className="create-course-form">
+					{console.log("VIdeos ----------------->isPublies:"+isPublished+"isPreview"+isPreview)}
+					<thead></thead>
+
+
+
+					{/*<div className="card" style={{ width: "18rem" }}>*/}
+					{/*	<img*/}
+					{/*		src={thumb ? thumb : ""}*/}
+					{/*		className="card-img-top"*/}
+					{/*		alt="..."*/}
+					{/*	/>*/}
+					{/*	<div className="card-body">*/}
+					{/*		<h5 className="card-title">{title}</h5>*/}
+					{/*		<button*/}
+					{/*			onClick={() => onDelete(videoId)}*/}
+					{/*			className="btn btn-danger"*/}
+					{/*		>*/}
+					{/*			Delete*/}
+					{/*		</button>*/}
+					{/*	</div>*/}
+					{/*</div>*/}
+
+					{/*<tbody>*/}
+					{/*{courses.length > 0 ? (*/}
+					{/*	courses.map((course) => (*/}
+					{/*		<CourseRow*/}
+					{/*			key={course.id}*/}
+					{/*			{...course}*/}
+					{/*			onHome={() =>*/}
+					{/*				handleCourseHome(*/}
+					{/*					course.id*/}
+					{/*				)*/}
+					{/*			}*/}
+					{/*			onHomeRemove={() =>*/}
+					{/*				handleCourseRemoveHome(*/}
+					{/*					course.id*/}
+					{/*				)*/}
+					{/*			}*/}
+					{/*		/>*/}
+					{/*	))*/}
+					{/*) : (*/}
+					{/*	<tr>*/}
+					{/*		<td*/}
+					{/*			colSpan="7"*/}
+					{/*			className="text-center py-3"*/}
+					{/*		>*/}
+					{/*			Empty!*/}
+					{/*		</td>*/}
+					{/*	</tr>*/}
+					{/*)}*/}
+					{/*</tbody>	*/}
+			<tbody className="tbody,td,tfoot,th,thead,tr">
+					<tr className="videoDetailrows">
+						<td scope="col" style={{width: "12rem"}}>{sessionOrder}</td>
+						<td scope="col" style={{width: "15rem"}}>{sessionTitle} </td>
+						<td scope="col" style={{width: "12rem"}}>3 hr 50 min</td>
+						<td scope="col" style={{width: "12rem"}}>{isPreview.toString()}</td>
+						<td scope="col" style={{width: "12rem"}}>{isPublished.toString()}</td>
+						<td scope="col" style={{width: "12rem"}}>
+							<div className="btn-group">
+
+							</div>
+
+						</td>
+					</tr>
+</tbody>
+
+
+
 				</div>
-			</div>
+			</table>
 		</div>
+
 	);
 };
 
 export default CourseVideos;
+//className="col-lg-3 col-md-6"
