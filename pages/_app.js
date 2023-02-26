1e87fbefb6c7141cf7d87dd4ec1fd93a2aff7be2
SoniPratham <d21it173@charusat.edu.in>
2023-02-26 11:09:49 +0530

    PAC 21 :Debug and make effective state management

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,5 @@
-import React, {useContext, useEffect, useState} from "react";
-import { Provider } from "react-redux";
-import { useStore } from "../store";
-import { parseCookies, destroyCookie } from "nookies";
-import axios from "axios";
-import { redirectUser } from "@/utils/auth";
-import baseUrl2 from "@/utils/baseUrl2";
+import React, { useEffect} from "react";
+import { parseCookies } from "nookies";
 import "../styles/bootstrap.min.css";
 import "../styles/animate.min.css";
 import "../styles/boxicons.min.css";
@@ -52,7 +47,6 @@ function MyApp({ Component, pageProps }) {
 			Router.push('/auth');
 		}
 	}, []);
-	// const store = useStore(pageProps.initialReduxState);
 	return (
 		<AppContext.Provider value={{ user, setUser }}>
 			<Layout>
@@ -62,84 +56,5 @@ function MyApp({ Component, pageProps }) {
 	);
 }
 
-
-MyApp.getInitialProps = async ({ Component, ctx }) => {
-	let pageProps = {};
-	console.log("First Time --------------->"+Component.user);
-	console.log("Authorized -------------------->"+isAuthorized+"\n");
-	// if (!isAuthorized) {
-	// 	// if a user not logged in then user can't access those pages
-	// 	const isProtectedRoute =
-	// 		ctx.pathname === "/profile/basic-information" ||
-	// 		ctx.pathname === "/profile/photo" ||
-	// 		ctx.pathname === "/checkout" ||
-	// 		ctx.pathname === "/become-an-instructor" ||
-	// 		ctx.pathname === "/learning/my-courses" ||
-	// 		ctx.pathname === "/instructor/courses" ||
-	// 		ctx.pathname === "/admin" ||
-	// 		ctx.pathname === "/admin/instructor" ||
-	// 		ctx.pathname === "/admin/students" ||
-	// 		ctx.pathname === "/admin/partners" ||
-	// 		ctx.pathname === "/admin/testimonials" ||
-	// 		ctx.pathname === "/admin/categories" ||
-	// 		ctx.pathname === "/checkout" ||
-	// 		ctx.pathname === "/learning/wishlist";
-	//
-	// 	console.log("ohh-----------------<<---"+cookies.get("charuvidhya_users_token")+"--->>");
-	// 	console.log("ohh-----------------<<---"+Component.charuvidhya_users_token+"--->>");
-	// 	if (isProtectedRoute) {
-	// 		const options = {
-	// 			headers: {
-	// 				'Authorization': 'Bearer ' +Component.charuvidhya_users_token,
-	// 				'Content-Type': 'application/json',
-	// 			}
-	// 		};
-	// 		const response = await fetch(`${baseUrl2}/api/account`, options);
-	// 		const jsonData = await response.json();
-	// 		if(typeof jsonData.firstName ==="undefined"){
-	// 			redirectUser(ctx, "/auth");
-	// 		}
-	// 		else{
-	//
-	// 		}
-	// 	}
-	// } else {
-		// if a user logged in then user can't access those pages
-		// const ifLoggedIn =
-		// 	ctx.pathname === "/auth" || ctx.pathname === "/reset-password";
-		// if (ifLoggedIn) {
-		// 	redirectUser(ctx, "/");
-		// }
-
-
-
-
-
-		// try {
-		// 	if (!(typeof cookie.get('charuvidhya_users_token')==="undefined")){
-		// 		const options = {
-		// 			headers: {
-		// 				'Authorization': 'Bearer ' + cookie.get('charuvidhya_users_token'),
-		// 				'Content-Type': 'application/json',
-		// 			}
-		// 		};
-		// 		const response = await fetch(`${baseUrl2}/api/account`, options);
-		// 		const jsonData = await response.json();
-		//
-		// 		// if (!user) {
-		// 		// 	destroyCookie(ctx, "charuvidhya_users_token");
-		// 		// 	redirectUser(ctx, "/auth");
-		// 		// }
-		// 		pageProps.user = jsonData;
-		// 	}
-		// } catch (err) {
-		// 	destroyCookie(ctx, "charuvidhya_users_token");
-		// 	// redirectUser(ctx, "/");
-		// }
-	// }
-	return {
-		pageProps,
-	};
-};
 
 export default MyApp;
