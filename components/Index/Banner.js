13506cf99f7108e3274352e004d5dd1473d305fe
Khushivithlani2 <khushivithlani2@gmail.com>
2023-03-07 02:34:00 +0530

    add total course and total enrolled student in home page

--- components/Index/Banner.js
+++ components/Index/Banner.js
@@ -1,10 +1,44 @@
 import Link from "next/link";
-import React from "react";
+import React, {useEffect, useState} from "react";
 import SearchForm from "@/components/_App/SearchForm";
 import AnimatedCharacters from "./BannerText";
 import { motion } from "framer-motion";
+import baseUrl2 from "@/utils/baseUrl2";
 
 const Banner = () => {
+
+	const [students,setStudents] = useState(0);
+	const [approvedCourses,setApprovedCourses] = useState(0);
+
+	const fetchData = async () => {
+		const res = await fetch(`${baseUrl2}/api/admin/coreMetaData`)
+		// console.log("Token is --> "+charuvidhya_users_token)
+		const {
+			students,
+			approvedCourses,
+		} = await res.json();
+		setStudents(students)
+		setApprovedCourses(approvedCourses)
+		console.log("Student : ------------->"+students+"Courses----------------->"+approvedCourses);
+		if (status === "FORBIDDEN"){
+			toast.error("Access Denied, You do not have access to view this page", {
+				style: {
+					border: "1px solid #ff0033",
+					padding: "16px",
+					color: "#ff0033",
+				},
+				iconTheme: {
+					primary: "#ff0033",
+					secondary: "#FFFAEE",
+				},
+			})
+			Router.push("/");
+		}
+	}
+
+	useEffect(() => {
+		fetchData();
+	}, []);
 	const headingText = [
 		{
 			type: "heading",
@@ -66,8 +100,8 @@ const Banner = () => {
 								animate="visible"
 								variants={pVariants}
 							>
-								We have <span>40k+</span> Online courses &{" "}
-								<span>500K+</span> Online registered student.
+								We have <span>{approvedCourses}+</span> Online courses &{" "}
+								<span>{students}+</span> Online registered student.
 								Find your desired Courses from them.
 							</motion.p>
 
@@ -96,7 +130,7 @@ const Banner = () => {
 								</li>
 								<li>
 									<p>
-										500K+ People already trusted us.{" "}
+										{students}+ People already trusted us.{" "}
 										<Link href="/courses">
 											<a className="read-more">
 												View Courses{" "}
