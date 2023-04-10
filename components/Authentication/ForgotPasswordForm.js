5405e8005637435e464987f14743e7b7d11cb44b
Kirtanshah2303 <shahkirtanv2002@gmail.com>
2023-04-10 17:24:37 +0530

    ForgetPassword and ResetPassword Integrated with backend API and working v1

--- components/Authentication/ForgotPasswordForm.js
+++ components/Authentication/ForgotPasswordForm.js
@@ -3,15 +3,56 @@ import {motion} from "framer-motion";
 import Button from "@/utils/Button";
 import React from "react";
 import Link from "next/link";
+import baseUrl2 from "@/utils/baseUrl2";
+import toast from "react-hot-toast";
+
+const INITIAL_USER = {
+    email: "",
+};
+
 const ForgotPasswordForm = ()=>{
 
-    // handleSubmit {
-    //     e.preventDefault();
-    // }
+    const [user, setUser] = React.useState(INITIAL_USER);
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setUser((prevState) => ({ ...prevState, [name]: value }));
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            const url = `${baseUrl2}/api/account/reset-password/init?mail=${user.email}`;
+            const response = await axios.post(url);
+            toast.success("Password Reset link is sent to your email", {
+                style: {
+                    border: "1px solid #4BB543",
+                    padding: "16px",
+                    color: "#4BB543",
+                },
+                iconTheme: {
+                    primary: "#4BB543",
+                    secondary: "#FFFAEE",
+                },
+            });
+        }catch (e) {
+            toast.error("Something went wrong", {
+                style: {
+                    border: "1px solid #ff0033",
+                    padding: "16px",
+                    color: "#ff0033",
+                },
+                iconTheme: {
+                    primary: "#ff0033",
+                    secondary: "#FFFAEE",
+                },
+            });
+        }
+    }
 
     return (
         <motion.form
-           // onSubmit={handleSubmit}
+           onSubmit={handleSubmit}
             initial={{ scale: 0 }}
             animate={{ scale: 1 }}
             exit={{ scale: 0 }}
@@ -22,9 +63,9 @@ const ForgotPasswordForm = ()=>{
                     type="email"
                     className="form-control"
                     placeholder="Email"
-                    name="username"
-                   // value={user.username}
-                    //onChange={handleChange}
+                    name="email"
+                   value={user.email}
+                    onChange={handleChange}
                     autoFocus
                 />
             </div>
